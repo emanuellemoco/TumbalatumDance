60684741ef1fb5fd51987c2b68db1d6cba332541
giovanacrl <giovanacrl@al.insper.edu.br>
2017-06-01 18:52:08 -0300

    Pega os eventos do teclado e checa se quando a pessoa clicou, ela acertou qual seta estava

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32) #configurações tela
 
 #adicionando imagem de fundo e mudando a configuração 956,560 tamanho img
-background_filename = 'pistadanca2.jpg'
+background_filename = '.\\imagens\\pistadanca2.jpg'
 background = pygame.image.load(background_filename).convert()
 
 #adicionando a nave
@@ -19,18 +19,21 @@ background = pygame.image.load(background_filename).convert()
 
 #lista_img = ['flechad.jpg','flechae.jpg','flechab.jpg','flechac.jpg']
 lista_img = [
-    pygame.image.load('flechad.jpg').convert_alpha(),
-    pygame.image.load('flechae.jpg').convert_alpha(),
-    pygame.image.load('flechab.jpg').convert_alpha(),
-    pygame.image.load('flechac.jpg').convert_alpha()
+    pygame.image.load('.\\flechas\\flechad.png').convert_alpha(),
+    pygame.image.load('.\\flechas\\flechae.png').convert_alpha(),
+    pygame.image.load('.\\flechas\\flechab.png').convert_alpha(),
+    pygame.image.load('.\\flechas\\flechac.png').convert_alpha()
 ]
 lista_personagem1 = ['desenho.png','desenho2.png' ]
+conta_tempo_pers = 0
+conta_desenho_pers = 0
 
 posicao_y = 460 
 posicao_x = 0 
 
 flechas = []
 flechas_position = []
+flechas_tipo = []
 
 n_flechas = 600
 for i in range(n_flechas):
@@ -42,40 +45,36 @@ for i in range(n_flechas):
     # Monta a posicao da flecha nova.
     flechas_position.append([0 - 200*i, posicao_y])
 
-
-flecha0_position = [0,posicao_y] #posição inicial
-flecha1_position = [0-200,posicao_y]
-flecha2_position = [0-400,posicao_y]
-flecha3_position = [0-600,posicao_y]
-flecha4_position = [0-800,posicao_y]
-flecha5_position = [0-1000,posicao_y]
-
-personagem0_position = [200,50]
-
-i = randint(0,3)
-n = randint(0,3)
-a = randint(0,3)
-b  = randint(0,3)
-c = randint(0,3)
-d = randint(0,3)
+    # Guarda tambem o tipo de flecha
+    flechas_tipo.append(k)
 
 pygame.display.set_caption('Tumbalatum Dance')
-pygame.mixer.music.load('Shape_of_You.wav')
+#pygame.mixer.music.load('Shape_of_You.wav')
+pygame.mixer.music.load('.\\musicas\\Explosao.wav') 
 pygame.mixer.music.play(0) #toca
 
 clock = pygame.time.Clock()
 
-personagem0_filename = lista_personagem1[0]
-personagem1_filename = lista_personagem1[1]
+personagens = []
+for filename in lista_personagem1:
+    novo_personagem = pygame.image.load(filename).convert_alpha()
+    personagens.append(novo_personagem)
 
-personagem0 = pygame.image.load(personagem0_filename).convert_alpha()
-personagem1 = pygame.image.load(personagem1_filename).convert_alpha()
+personagem_position = [200,50]
 
 speed = 10
 
 #loop para atualizar 
 while True:
+    tecla = None
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or \
+               event.key == pygame.K_LEFT or \
+               event.key == pygame.K_UP or \
+               event.key == pygame.K_DOWN:
+                tecla = event.key
+
         if event.type == QUIT:
             exit()
 
@@ -83,14 +82,29 @@ while True:
 
     for i in range(len(flechas_position)):
         flechas_position[i][0] += speed
-        if flechas_position[i][0] > -100 and flechas_position[i][0] < SCREEN_WIDTH:
+
+        pos_x = flechas_position[i][0]
+        if pos_x > -100 and pos_x < SCREEN_WIDTH:
             screen.blit(flechas[i], flechas_position[i])
 
+        if pos_x > 800 and pos_x < 900:
+            tipo = flechas_tipo[i]
 
+            if (tipo == 0 and tecla == pygame.K_RIGHT) or \
+               (tipo == 1 and tecla == pygame.K_LEFT) or \
+               (tipo == 2 and tecla == pygame.K_DOWN) or \
+               (tipo == 3 and tecla == pygame.K_UP):
+                if pos_x > 840 and pos_x < 860:
+                    print("Perfect!")
+                elif pos_x > 820 and pos_x < 880:
+                    print("Good!")
+                else:
+                    print("Ok!")
+                conta_desenho_pers = (conta_desenho_pers + 1) % len(personagens)
+            elif tecla != None:
+                print("Errou!")
 
-    screen.blit(personagem0, personagem0_position)
-    screen.blit(personagem1, personagem0_position)
+    screen.blit(personagens[conta_desenho_pers], personagem_position)
 
     pygame.display.update()
     time_passed = clock.tick(30)
-
